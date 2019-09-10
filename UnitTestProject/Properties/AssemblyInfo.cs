7915088a0ea76b7fb2864647937dd4d023074b67
Admin <Admin@DESKTOP-POQE336>
2019-09-10 15:00:36 +1200

    Add project files.

---
+++ UnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a11dd3f8-a66a-4484-8759-9691168be502")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using AthleteRace;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Get_Payout()
+        {
+            // Arrange
+            var pay = new Bet();
+
+            // Act
+            var actual = pay.Payout(1);
+
+            // Assert
+            Assert.AreEqual(0, actual);
+        }
+
+        [TestMethod]
+        public void Check_Bet_Placed()
+        {
+            // Arrange
+            var bettor = new Bettor();
+
+            // Act
+            var actual = bettor.PlaceABet(50, 1);
+
+            // Assert
+            Assert.AreEqual(false, actual);
+        }
+    }
+}
